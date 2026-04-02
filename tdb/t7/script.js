fc3b27d6f82de2762d4148082ca18c217c752561
Trần Tiến Đại <dai09112006@gmail.com>
2026-04-02 09:49:52 +0700

    Delete tdb directory

--- tdb/t7/script.js
+++
@@ -1,9 +0,0 @@
-function openCity(cityName) {
-  let x = document.getElementsByClassName("tab-content");
-
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-
-  document.getElementById(cityName).style.display = "block";
-}
